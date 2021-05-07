c6f910001f5726f4d1d19a45d13ac33afa94fbbf
Vincent malkowiak <vincent.malkowiak@iutnice.unice.fr>
2021-05-07 14:31:50 +0200

    pause et reprise de lecture v1 slider correction du lancement de plusieurs thread de lecture

--- core/gui_action.py
+++ core/gui_action.py
@@ -3,33 +3,50 @@ import threading
 from time import sleep
 from tkinter import filedialog
 
-def start(app):
-	actions_list = my_parser.parse(app.getFile().strip())
-
-	reader = threading.Thread(target=lambda:readAction(app,actions_list,True),name="reader", daemon=True)
+def start(appGui):
+	run = threading.Event()
+	reader = threading.Thread(target=readAction,args=(appGui,appGui.actions_list,run),name="reader", daemon=True)
 	reader.start()
+	appGui.start.configure(text = 'Stop', command = lambda : pause(appGui,run))
 
-def readAction(app, actions_list, run):
+
+def pause(appGui, run):
+	run.set()
+	changeButton(appGui)
+
+def changeButton(appGui):
+	appGui.start.configure(text = 'Run', command = lambda : start(appGui))
+
+def readAction(appGui, actions_list, run):
 	print("thread")
-	app.reset()
-	start = app.getStart()
+	appGui.reset()
+	start = appGui.getStart()
 	for action in actions_list[:start]:
-		if not run:
-			break
 		if action.type == "ins":
-			app.insert(action.text, action.position)
+			appGui.insert(action.text, action.position)
 		elif action.type == "back":
-			app.delete(action.text, action.position)
+			appGui.delete(action.text, action.position)
 
 	for action in actions_list[start:]:
-		if not run:
-			break
+		if run.is_set():
+			updateSlider(appGui, actions_list.index(action))
+			return
 		if action.type == "ins":
-			app.insert(action.text, action.position)
+			appGui.insert(action.text, action.position)
 		elif action.type == "back":
-			app.delete(action.text, action.position)
+			appGui.delete(action.text, action.position)
 		sleep(0.1)
+	updateSlider(appGui, len(actions_list))
+	changeButton(appGui)
 
-def openFile(app):
-	app.fileInput = filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("ses files","*.ses"),("all files","*.*")))
-	app.displayFile()
+def updateSlider(appGui, value):
+	appGui.updateStart(value)
+
+
+def openFile(appGui):
+	appGui.reset()
+	updateSlider(appGui, 0)
+	appGui.fileInput = filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("ses files","*.ses"),("all files","*.*")))
+	appGui.displayFile()
+	appGui.actions_list = my_parser.parse(appGui.getFile().strip())
+	appGui.setSliderMax(len(appGui.actions_list))
--- core/my_gui.py
+++ core/my_gui.py
@@ -8,13 +8,13 @@ class App(object):
         parent = tk.Tk()
         parent.geometry("400x300")
         self.root = parent
-        self.startingpoint = tk.Text(self.root, height = 1,width = 20,bg = "light yellow")
         self.root.title("Schnappi viewer")
         self.frame = tk.Frame(self.root)
-        self.label = tk.Label(self.root)
+        self.mainText = tk.Label(self.root,bg = "white", justify="left")
         self.fileLabel = tk.Label(self.root)
         self.chooseFile = tk.Button(self.root, text ="Open file", command = lambda: gui_action.openFile(self))
         self.start = tk.Button(self.root, text ="Run", command = lambda: gui_action.start(self))
+        self.slider = tk.Scale(self.root, from_=0, to=200,orient="horizontal")
         
         
     def getFile(self):
@@ -22,39 +22,46 @@ class App(object):
 
     def insert(self, char, position):
         print("Ajout : "+char)
-        char_list = list(self.label.cget("text"))
+        char_list = list(self.mainText.cget("text"))
         if char == "&#xa;":
             char = "\n"
 
         if len(char_list) > int(position):
             char_list[int(position)] = char
-            self.label.configure(text="".join(char_list))
+            self.mainText.configure(text="".join(char_list))
         else:
-            self.label.configure(text=self.label.cget("text") + char)
+            self.mainText.configure(text=self.mainText.cget("text") + char)
+
+
+    def setSliderMax(self, value):
+        self.slider.configure(to = value)
 
     def delete(self, char, position):
         print("Suppression : "+char)
-        char_list = list(self.label.cget("text"))
+        char_list = list(self.mainText.cget("text"))
         if char == "&#xa;":
             char = "\n"
         if len(char_list) > int(position) and char_list[int(position)] == char:
             char_list.pop(int(position))
-            self.label.configure(text = "".join(char_list))
+            self.mainText.configure(text = "".join(char_list))
     
     def reset(self):
-        self.label.configure(text = "")
+        self.mainText.configure(text = "")
 
     def displayFile(self):
         self.fileLabel.configure(text = self.fileInput)
 
     def getStart(self):
-        return int(self.startingpoint.get("1.0" ,"end").strip())
+        return self.slider.get()
+
+    def updateStart(self, value):
+        self.slider.set(value)
 
 
     def init(self):
         self.chooseFile.pack()
         self.fileLabel.pack()
         self.frame.pack()
-        self.startingpoint.pack()
         self.start.pack()
-        self.label.pack()
+        self.mainText.pack()
+        self.slider.pack()
